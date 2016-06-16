49bb820a13f88d4254d4f7d58f4337ead441c424
anujbhardwaj <bhardwaj.anuj.ab@gmail.com>
2016-06-16 11:55:36 +0530

    Create vertical-stacktable.js

---
+++ vertical-stacktable.js
@@ -0,0 +1,45 @@
+/**
+* vertical-stacktable.js
+* Author & copyright (c) 2016: Anuj Bhardwaj
+*
+* jQuery plugin for stacking horizontal table to vertical table
+*
+*/
+
+$('.ab-table-responsive').each(function(){
+
+	var table = document.getElementsByClassName('desktop-view');
+	var tr = $(this).find('tr');
+	var td;
+	var newTable = document.createElement('table');
+	for(var i=0; i<tr.length; i++){
+		td = tr[i].getElementsByTagName('td');
+		if(i === 0){
+			for(var j=0; j<td.length; j++){
+				var newTr = document.createElement('tr');
+				var newTd2 = document.createElement('td');
+				newTd2.innerHTML = td[j].innerHTML;
+				newTr.appendChild(newTd2);
+				newTable.appendChild(newTr);
+			}
+		}
+		else {
+			var oldtr = newTable.getElementsByTagName('tr');
+			for(var j=0; j<oldtr.length; j++){
+				var newTd2 = document.createElement('td');
+					newTd2.innerHTML = td[j].innerHTML;
+					oldtr[j].appendChild(newTd2);
+					newTable.appendChild(newTr);
+			}
+		}
+	}
+
+
+	$(this).before(newTable);
+	$(this).prev('table').addClass('ab-table');
+	$(this).prev('table').find('tr').addClass('ab-table-row');
+	$(this).prev('table').find('td').addClass('ab-table-col');
+	$(this).prev('table').find('tr').first().addClass('ab-table-row-main');
+	$(this).prev('table').find('td').addClass('ab-table-col');
+	$(this).prev('table').find('tr').first().find('td').addClass('ab-table-col-main');
+});
